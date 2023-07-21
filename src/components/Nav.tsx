35ba61073c62d015c2bfdf63e8e5f06b4e08331d
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-21 16:16:44 +0100

    all functionalities working on ProgressBar

---
+++ src/components/Nav.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import Link from 'next/link';
+import { v4 as uuidv4 } from 'uuid';
+import {
+  HomeIcon,
+  LockOpenIcon,
+  UserPlusIcon,
+} from '@heroicons/react/24/solid';
+
+type Nav = {
+  name: string;
+  title: string;
+  icon: object;
+  id: string;
+  link: string;
+};
+
+const NavLink: Nav[] = [
+  {
+    name: 'Home',
+    title: 'home',
+    icon: <HomeIcon />,
+    id: uuidv4(),
+    link: '/',
+  },
+
+  {
+    name: 'Register',
+    title: 'register',
+    icon: <UserPlusIcon />,
+    id: uuidv4(),
+    link: '/register',
+  },
+
+  {
+    name: 'Login',
+    title: 'login',
+    icon: <LockOpenIcon />,
+    id: uuidv4(),
+    link: '/login',
+  },
+];
+
+const Nav = (): React.ReactNode => {
+  return (
+    <ul className='bg-red-900'>
+      {NavLink.map((nav) => {
+        return (
+          <li
+            title={nav.title}
+            className='nav-item'
+            key={nav.id}
+          >
+            <Link
+              href={nav.link}
+              className='nav-link  text-black'
+            >
+              {nav.name}
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default Nav;
--- src/components/Progressbar.tsx
+++ src/components/Progressbar.tsx
@@ -10,12 +10,13 @@ export function ProgressBar() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
 
-  useEffect(() => {
-    const url = pathname + searchParams.toString();
-    NProgress.start(url);
-    NProgress.done(url);
-    console.log(url);
-
+    useEffect(() => {
+      
+      const url: any = pathname + searchParams.toString();
+      NProgress.start();
+      NProgress.done(url);
+      console.log(url);
+      
     // You can now use the current URL
     // ...
   }, [pathname, searchParams]);
