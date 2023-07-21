fe8422e2bc665d8748525a69160f56a9ca2d57e7
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-21 15:33:57 +0100

    working on progressBar

---
+++ src/components/Progressbar.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { useEffect } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation';
+import NProgress from 'nprogress';
+
+NProgress.configure({ easing: 'ease', speed: 1000, minimum: 0.1 });
+
+export function ProgressBar() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const url = pathname + searchParams.toString();
+    NProgress.start(url);
+    NProgress.done(url);
+    console.log(url);
+
+    // You can now use the current URL
+    // ...
+  }, [pathname, searchParams]);
+
+  return null;
+}
