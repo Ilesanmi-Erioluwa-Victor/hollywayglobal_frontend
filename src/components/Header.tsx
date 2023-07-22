d30a0bcb98671aeb58c92415a1fad65fd0879cef
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-22 06:07:54 +0100

    Working on Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,12 +6,22 @@ const Header = (): JSX.Element => {
   return (
     <header className='px-[1rem] flex flex-col gap-[5px]'>
       <main className='text-black text-[13px]'>
-        <section>
-          <div>
-            <h3>Hi</h3>
-            <Link href={'/login'}>Sign in</Link>
+        <section className='flex'>
+          <div className='flex pr-[5px] gap-[5px]'>
+            <h3>Hi!</h3>
+            <Link
+              href={'/login'}
+              className='block underline text-blue-700'
+            >
+              Sign in
+            </Link>
             <span>or</span>
-            <Link href={'/register'}>Sign up</Link>
+            <Link
+              href={'/register'}
+              className='block underline text-blue-700'
+            >
+              register
+            </Link>
           </div>
           <p>
             Free shipping for all Order of <span>$249</span>
