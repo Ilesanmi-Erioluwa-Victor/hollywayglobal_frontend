1f1a0da08519a69348bdc075efa2dcd1ae41e83f
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-07-22 05:54:19 +0100

    Working on Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import Nav from './Nav'
+import React from 'react';
+import Nav from './Nav';
+import Link from 'next/link';
 
 const Header = () => {
   return (
-      <header>
-          <main>
-              <div></div>
-          </main>
+    <header>
+      <main>
+        <section>
+          <div>
+            <h3>Hi</h3>
+            <Link href={'/login'}>Sign in</Link>
+            <span>or</span>
+            <Link href={'/register'}>Sign up</Link>
+          </div>
+          <p>
+            Free shipping for all Order of <span>$249</span>
+          </p>
+        </section>
+      </main>
       <Nav />
     </header>
-  )
-}
+  );
+};
 
-export default Header
-
+export default Header;
