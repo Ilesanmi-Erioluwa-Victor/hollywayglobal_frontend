73c1feb5e5339e8855c428c5982b2c5748326b14
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-21 20:56:13 +0100

    working on Header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+
+      colors: {
+        transparent: 'transparent',
+        primary_green: '#0D5520',
+        primary_yellow: '#FDF1C9',
+        secondary_yellow: '#FFC42D73',
+        dark_blue: '#1C1D36',
+        white_2: '#F8F8F8',
+        white_3: '#F0FBF3',
+        light_green: '#D6ECDC',
+        dark_500: 'rgba(0, 0, 0, 0.67)',
+        primary_grey: '#B6BBB2',
+        white: '#FFFFFF',
+        grey: '#BBBBC3',
+        light_grey: '#B6BBB2',
+        sunglow: '#0D5520',
+        secondary_yellow_2: '#FFC42D',
+        lighter_green: 'rgba(214, 236, 220, 0.4)',
+        contact_grey: '#F8F8F8',
+      },
+    },
   },
   plugins: [],
 };
-
