5b99d2739e8470d666754f0ecd1d4449841e9c76
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-21 21:47:12 +0100

    Working on register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,13 @@
 const Register = () => {
-  return <div className="bg-red-900 paddTop">
+  return <div className="bg-red-900 paddTop flex">
 
-      
+      <div className="h-[10rem] bg-yellow-700 w-[50%]">fgfbnbn</div>
+
+      <div>
+          <form>
+              <h3>Form</h3>
+          </form>
+      </div>
   </div>;
 };
 
