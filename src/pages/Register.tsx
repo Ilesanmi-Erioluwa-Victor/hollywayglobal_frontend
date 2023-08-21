42009a7917a7470967af6486bb9a432f4969a969
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-21 22:23:27 +0100

    working on Header

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,26 +3,77 @@ import TextField from '@mui/material/TextField';
 
 const Register = () => {
   return (
-    <div className='bg-red-900 paddTop flex'>
-      <div className='h-[10rem] bg-yellow-700 w-[50%]'>fgfbnbn</div>
+    <div className=' paddTop flex'>
+      <div className='h-[10rem] w-[55%]'>fgfbnbn</div>
 
-      <div className='padd'>
-        <form>
+      <div className='padd w-[45%] bg-green-900'>
+        <form className='flex flex-col gap-[1rem]'>
           <h3 className='font-[500] text-[2rem]'>Create an account</h3>
           <p className='font-[400] text-[0.8rem]'>Enter your details below</p>
-
-          <Box
-            sx={{
-              width: 500,
-              maxWidth: '100%',
-            }}
-          >
-            <TextField
-              fullWidth
-              label='fullWidth'
-              id='fullWidth'
-            />
-          </Box>
+          <div className='flex flex-col gap-[1rem]'>
+            {' '}
+            <Box
+              sx={{
+                width: 500,
+                maxWidth: '100%',
+              }}
+            >
+              <TextField
+                fullWidth
+                label='fullWidth'
+                id='fullWidth'
+              />
+            </Box>
+            <Box
+              sx={{
+                width: 500,
+                maxWidth: '100%',
+              }}
+            >
+              <TextField
+                fullWidth
+                label='fullWidth'
+                id='fullWidth'
+                name=''
+              />
+            </Box>
+            <Box
+              sx={{
+                width: 500,
+                maxWidth: '100%',
+              }}
+            >
+              <TextField
+                fullWidth
+                label='fullWidth'
+                id='fullWidth'
+              />
+            </Box>
+            <Box
+              sx={{
+                width: 500,
+                maxWidth: '100%',
+              }}
+            >
+              <TextField
+                fullWidth
+                label='fullWidth'
+                id='fullWidth'
+              />
+            </Box>
+            <Box
+              sx={{
+                width: 500,
+                maxWidth: '100%',
+              }}
+            >
+              <TextField
+                fullWidth
+                label='fullWidth'
+                id='fullWidth'
+              />
+            </Box>
+          </div>
         </form>
       </div>
     </div>
