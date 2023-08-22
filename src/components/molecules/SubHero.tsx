5563c5704917bfae745bc5c6d2274580c2c30a46
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-22 23:29:43 +0100

    Working on LandingPage

--- src/components/molecules/SubHero.tsx
+++ src/components/molecules/SubHero.tsx
@@ -1,9 +1,66 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import Image from '../../assets/dark_800.png';
 import Image1 from '../../assets/signup.png';
 import Image2 from '../../assets/react.svg';
 
 const SubHero = () => {
-  const slides = [Image, Image1, Image2];
+  const products = [
+    {
+      name: 'Product Name',
+      id: uuidv4(),
+      image: Image,
+    },
+
+    {
+      name: 'Product Name',
+      id: uuidv4(),
+      image: Image2,
+    },
+
+    {
+      name: 'Product Name',
+      id: uuidv4(),
+      image: Image1,
+    },
+
+    {
+      name: 'Product Name',
+      id: uuidv4(),
+      image: Image,
+    },
+
+    {
+      name: 'Product Name',
+      id: uuidv4(),
+      image: Image1,
+    },
+
+    {
+      name: 'Product Name',
+      id: uuidv4(),
+      image: Image2,
+    },
+
+    {
+      name: 'Product Name',
+      id: uuidv4(),
+      image: Image,
+    },
+
+    {
+      name: 'Product Name',
+      id: uuidv4(),
+      image: Image1,
+    },
+
+    {
+      name: 'Product Name',
+      id: uuidv4(),
+      image: Image2,
+    },
+  ];
+
   return (
     <div className='bg-white flex flex-wrap'>
       <div>Hello</div>
