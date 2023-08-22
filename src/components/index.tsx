881aa22658360f7061578d33440da6e4db2d808e
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-22 23:16:03 +0100

    Working on LandingPage

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,3 +2,4 @@ export { default as Header } from './molecules/Header';
 export { default as Footer } from './molecules/Footer';
 export { default as ImagePage } from './Image';
 export { default as Hero } from './molecules/Hero';
+export { default as SubHero } from './molecules/SubHero';
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import SubHero from '../components';
 import { Hero } from '../components';
 
 const Home = () => {
   return (
     <div className='padd bg-slate-100'>
       <Hero />
+      <SubHero />
     </div>
   );
 };
