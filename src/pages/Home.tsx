668d772114e733ea009b3bf720294160917d28df
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-22 23:55:30 +0100

    Working on LandingPage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
-import { Hero, SubHero } from '../components';
+import { Hero, SubHero, SubHeroTwo } from '../components';
 
 const Home = () => {
   return (
     <div className='padd bg-slate-100 flex flex-col gap-4'>
       <Hero />
       <SubHero />
-      <SubHero />
+      <SubHeroTwo />
 
       <SubHero />
       <SubHero />
