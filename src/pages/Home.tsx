be235f30a0a0a4356f2946fb3e097e56fb4a03d4
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-22 23:47:51 +0100

    Working on LandingPage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,10 @@ const Home = () => {
     <div className='padd bg-slate-100 flex flex-col gap-4'>
       <Hero />
       <SubHero />
+      <SubHero />
+
+      <SubHero />
+      <SubHero />
     </div>
   );
 };
