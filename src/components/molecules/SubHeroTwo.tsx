267552780691c4ae29670bfbd885b58000e94c3e
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-22 23:53:29 +0100

    Working on LandingPage

---
+++ src/components/molecules/SubHeroTwo.tsx
@@ -0,0 +1,8 @@
+import '~slick-carousel/slick/slick.css';
+import '~slick-carousel/slick/slick-theme.css';
+
+const SubHeroTwo = () => {
+  return <div>SubHeroTwo</div>;
+};
+
+export default SubHeroTwo;
