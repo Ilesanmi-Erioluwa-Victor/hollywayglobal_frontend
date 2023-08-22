da518f7215dbb8c360e7090ab77b7b2b40abd713
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-22 14:14:17 +0100

    Working on Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,81 @@
-import { ImagePage } from "../components";
+import { ChangeEvent, useState } from 'react';
+import Button from '@mui/material/Button';
+
+import { Link } from 'react-router-dom';
+
+import { ImagePage } from '../components';
+import { FieldSet } from '../components/atoms';
 
 const Login = () => {
+  const [data, setData] = useState({
+    firstName: '',
+    lastName: '',
+    password: '',
+    email: '',
+    mobile: '',
+  });
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData({ ...data, [name]: value.trim() });
+  };
+
+  const handleInputSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='paddTop flex'>
-          <ImagePage />
-          
-          
+      <ImagePage />
+
+      <div className='padd w-[100%] md:w-[45%] py-[1rem] paddTop self-center place-self-center content-center'>
+        <form
+          className='flex flex-col gap-[1rem]'
+          onSubmit={handleInputSubmit}
+        >
+          <h3 className='text-[1.8rem] font-[500]  tracking-[1.44px] md:text-[1.6rem] md:tracking-[0px] lg:text-[2rem]'>
+            Log in to Exclusive
+          </h3>
+          <p className='font-[400] text-[1rem]'>Enter your details below</p>
+          <div className='flex flex-col gap-[1rem]'>
+            <FieldSet
+              name='email'
+              label='email'
+              value={data.email}
+              onChange={handleInputChange}
+              id='email'
+              type='email'
+            />
+
+            <FieldSet
+              name='password'
+              label='password'
+              value={data.password}
+              onChange={handleInputChange}
+              id='password'
+              type='password'
+            />
+
+            <Button
+              variant='contained'
+              size='large'
+              type='submit'
+              className='p-[1rem] text-[2rem]'
+            >
+              Create Account
+            </Button>
+          </div>
+        </form>
+      </div>
+      <p className='text-[1rem] flex justify-end items-center pt-2 gap-4'>
+        Already have account? <Link to={'/login'}>Login</Link>
+      </p>
     </div>
   );
 };
