5ec15793fda017ed54126ef1ced58123e0cd0a4e
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-22 13:59:50 +0100

    Working on Login page

---
+++ src/components/Image.tsx
@@ -0,0 +1,14 @@
+import Image from '../assets/signup.png';
+
+const ImagePage = () => {
+  return (
+    <div className='hidden md:block w-[55%] '>
+      <img
+        src={Image}
+        alt=''
+      />
+    </div>
+  );
+};
+
+export default ImagePage;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,3 @@
 export { default as Header } from './molecules/Header';
 export { default as Footer } from './molecules/Footer';
+export { default as ImagePage } from './Image';
