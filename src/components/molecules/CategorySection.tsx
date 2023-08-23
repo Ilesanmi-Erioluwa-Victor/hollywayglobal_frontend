209e06c240e22808bedae491b282150c6205a7d8
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-23 22:10:33 +0100

    Working on CategorySection

--- src/components/molecules/CategorySection.tsx
+++ src/components/molecules/CategorySection.tsx
@@ -1,13 +1,15 @@
+import { BsPhone } from 'react-icons/bs';
+import { HiOutlineComputerDesktop } from 'react-icons/hi2';
+
 const CategorySection = () => {
   return (
     <div className='bg-white  p-4 shadow-md rounded-md'>
-
       <div className='flex gap-6 items-center'>
         <span className='block w-6 rounded-sm h-12 bg-[#DB4444]'></span>
         <span className='block text-[#DB4444]'>categories</span>
       </div>
 
-      <h2 className="mt-8 font-[400] text-lg mb-10">Browse By Category</h2>
+      <h2 className='mt-8 font-[400] text-lg mb-10'>Browse By Category</h2>
 
       <div></div>
     </div>
