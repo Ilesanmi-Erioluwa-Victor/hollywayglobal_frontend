d241d5fcdde792e43b213259e899a15a299606d1
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-23 22:04:19 +0100

    Working on CategorySection

--- src/components/molecules/CategorySection.tsx
+++ src/components/molecules/CategorySection.tsx
@@ -1,7 +1,10 @@
 const CategorySection = () => {
   return (
-    <div className='bg-white flex flex-wrap gap-3 items-center justify-center p-4 shadow-md rounded-md'>
-      CategorySection
+    <div className='bg-white  p-4 shadow-md rounded-md'>
+      <div className='flex gap-6 items-center'>
+        <span className='block w-6 rounded-sm h-12 bg-[#DB4444]'></span>
+        <span className='block text-[#DB4444]'>Categories</span>
+      </div>
     </div>
   );
 };
