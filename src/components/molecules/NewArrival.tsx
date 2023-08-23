ef94d6ff9b927681b174c3cab0dde70c2e470ece
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-23 23:06:54 +0100

    Working on CategorySection

---
+++ src/components/molecules/NewArrival.tsx
@@ -0,0 +1,9 @@
+import Image from '../../assets/dark_800.png';
+import Image1 from '../../assets/signup.png';
+import Image2 from '../../assets/react.svg';
+
+const NewArrival = () => {
+  return <div></div>;
+};
+
+export default NewArrival;
