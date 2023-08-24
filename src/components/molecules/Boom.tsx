4f527564bdeab8361e969e6d211e42c4ddfbf207
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-24 20:54:51 +0100

    working on responsiveness

--- src/components/molecules/Boom.tsx
+++ src/components/molecules/Boom.tsx
@@ -2,16 +2,16 @@ import ImageBoom from '../../assets/boom.png';
 
 const Boom = () => {
   return (
-    <div className='bg-black  p-[4rem] shadow-md rounded-md flex text-white items-center justify flex-wrap md:flex-nowrap'>
+    <div className='bg-black  p-[1rem] md:p-[4rem] shadow-md rounded-md flex text-white items-center justify flex-wrap md:flex-nowrap'>
       <div>
-        <h3 className='text-lg'>Categories</h3>
-        <h1 className='text-[3rem]'>Enhance Your Music Experience</h1>
+        <h3 className='text-base md:text-lg'>Categories</h3>
+        <h1 className='text-[2rem] md:text-[3rem]'>Enhance Your Music Experience</h1>
         <button className='bg-[#00FF66] py-4 px-[3rem] rounded-sm mt-5'>
           Buy Now
         </button>
       </div>
 
-      <div className='cum-bg p-6 relative overflow-hidden'>
+      <div className='cum-bg p-6 relative overflow-hidden w-[100%]'>
         <img
           src={ImageBoom}
           alt=''
