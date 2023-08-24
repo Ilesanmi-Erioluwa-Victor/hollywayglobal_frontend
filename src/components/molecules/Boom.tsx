d537df4f879ebc8ecc248e6f7741a516501cabf5
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-24 20:03:20 +0100

    working on responsiveness

--- src/components/molecules/Boom.tsx
+++ src/components/molecules/Boom.tsx
@@ -2,10 +2,13 @@ import ImageBoom from '../../assets/boom.png';
 
 const Boom = () => {
   return (
-    <div className='bg-black  p-[4rem] shadow-md rounded-md flex'>
-      <div></div>
-
+    <div className='bg-black  p-[4rem] shadow-md rounded-md flex text-white items-center justify'>
       <div>
+        <h3>Categories</h3>
+        <h1 className='text-[3rem]'>Enhance Your Music Experience</h1>
+      </div>
+
+      <div className=''>
         <img
           src={ImageBoom}
           alt=''
