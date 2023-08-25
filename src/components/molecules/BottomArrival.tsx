44de3de08a41c1594dd96583972961214946cf3f
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-25 21:15:10 +0100

    working on complex layout

--- src/components/molecules/BottomArrival.tsx
+++ src/components/molecules/BottomArrival.tsx
@@ -3,13 +3,13 @@ import Image from '../../assets/boom.png';
 const BottomArrival = () => {
   return (
     <div className='grid p-[1rem] gap-x-[1rem] gap-y-[5px] grid-cols-[repeat(12,1fr)] bg-white grid-rows-[repeat(10,1fr)] text-white'>
-      <div className='col-[1_/_7] row-[1_/_-1] rounded-md'>
+      <div className='col-[1_/_7] row-[1_/_-1] bg-[#f0f0f0] rounded-md'>
         <img
           src={Image}
           alt=''
         />
       </div>
-      <div className='col-[7_/_-1] row-[1_/_7] bg-orange-600'>
+      <div className='col-[7_/_-1] row-[1_/_7] bg-[#f0f0f0] rounded-md'>
         <img
           src={Image}
           alt=''
@@ -17,14 +17,14 @@ const BottomArrival = () => {
         />
       </div>
 
-      <div className='col-[7_/_10] row-[7_/_-1] bg-green-600'>
+      <div className='col-[7_/_10] row-[7_/_-1]bg-[#f0f0f0] rounded-md'>
         <img
           src={Image}
           alt=''
         />
       </div>
 
-      <div className='bg-red-900 col-[10_/_-1] row-[7_/_-1]'>
+      <div className='rounded-md col-[10_/_-1] row-[7_/_-1]bg-[#f0f0f0]'>
         <img
           src={Image1}
           alt=''
