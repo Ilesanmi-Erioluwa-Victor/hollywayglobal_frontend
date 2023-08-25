2e0a1caabb2bc05c538080ff9494a8c66c2d5c5c
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-25 21:06:01 +0100

    working on complex layout

--- src/components/molecules/BottomArrival.tsx
+++ src/components/molecules/BottomArrival.tsx
@@ -1,17 +1,34 @@
-
-
+import Image1 from '../../assets/signup.png';
+import Image from '../../assets/boom.png';
 const BottomArrival = () => {
   return (
-    <div className='grid grid-cols-[repeat(12,1fr)] bg-black gap-[10px] grid-rows-[repeat(12,1fr)] text-white'>
-      <div className='bg-white col-[1_/_7] row-[1_/_-1]'>First</div>
-      <div className='col-[7_/_-1] row-[1_/_7] bg-orange-600'>second</div>
+    <div className='grid p-[1rem] gap-x-[1rem] gap-y-[5px] grid-cols-[repeat(12,1fr)] bg-black grid-rows-[repeat(10,1fr)] text-white'>
+      <div className='bg-white col-[1_/_7] row-[1_/_-1]'>
+        <img src={Image1} alt="" />
+      </div>
+      <div className='col-[7_/_-1] row-[1_/_7] bg-orange-600'>
+        <img
+          src={Image}
+          alt=''
+          className='carousel-image'
+        />
+      </div>
 
-      <div>Third</div>
+      <div className='col-[8_/_12] row-[7_/_-1] bg-green-600'>
+        <img
+          src={Image}
+          alt=''
+        />
+      </div>
 
-      <div>Fourth</div>
+      <div className='bg-red-900 col-[12_/_-1] row-[7_/_15]'>
+        {/* <img
+          src={Image1}
+          alt=''
+        /> */}
+      </div>
     </div>
   );
-}
+};
 
-export default BottomArrival
-
+export default BottomArrival;
