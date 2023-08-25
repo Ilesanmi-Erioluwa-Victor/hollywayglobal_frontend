a23bad6f2139cac5e34136f3c6538f0031a44af4
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-25 19:53:25 +0100

    First commit

--- src/components/molecules/BottomArrival.tsx
+++ src/components/molecules/BottomArrival.tsx
@@ -2,10 +2,17 @@
 
 const BottomArrival = () => {
   return (
-    <div>
-      BottomArrival
+    <div className='grid grid-cols-[repeat(12,1fr)] bg-black gap-[10px] grid-rows-[repeat(12,1fr)] text-white'>
+      <div className="">
+        First
+      </div>
+      <div>second</div>
+
+      <div>Third</div>
+
+      <div>Fourth</div>
     </div>
-  )
+  );
 }
 
 export default BottomArrival
