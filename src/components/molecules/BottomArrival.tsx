428ff7df637e87b668a4e957eda14ded1dc4e0db
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-25 20:03:23 +0100

    working on complex layout

--- src/components/molecules/BottomArrival.tsx
+++ src/components/molecules/BottomArrival.tsx
@@ -3,10 +3,8 @@
 const BottomArrival = () => {
   return (
     <div className='grid grid-cols-[repeat(12,1fr)] bg-black gap-[10px] grid-rows-[repeat(12,1fr)] text-white'>
-      <div className="">
-        First
-      </div>
-      <div>second</div>
+      <div className='bg-white col-[1_/_7] row-[1_/_-1]'>First</div>
+      <div className='col-[7_/_-1] row-[1_/_7] bg-orange-600'>second</div>
 
       <div>Third</div>
 
