3df982aebe6de60f0116b57bdb3841cf39012e68
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-26 14:48:35 +0100

    Working on user profile dropdown

--- src/components/molecules/BottomArrival.tsx
+++ src/components/molecules/BottomArrival.tsx
@@ -5,10 +5,12 @@ const BottomArrival = () => {
     <div className='bg-white  p-4 shadow-md rounded-md'>
       <div className='flex gap-6 items-center'>
         <span className='block w-6 rounded-sm h-12 bg-[#DB4444]'></span>
-        <span className='block text-[#DB4444]'>categories</span>
+        <span className='block text-[#DB4444]'>Newest Products</span>
       </div>
 
-      <h2 className='mt-8 font-[400] text-lg mb-10'>Browse By Category</h2>
+      <h2 className='mt-8 font-[400] text-lg mb-4'>
+        Buy now and Get Whooping Discount
+      </h2>
       <div className='grid p-[1rem] gap-x-[1rem] gap-y-[5px] grid-cols-[repeat(12,1fr)]  grid-rows-[repeat(10,1fr)]'>
         <div className='col-[1_/_7] row-[1_/_-1] rounded-md bg-[#f0f0f0] flex items-center justify-center p-4'>
           <img
@@ -16,7 +18,7 @@ const BottomArrival = () => {
             alt=''
           />
         </div>
-        <div className='col-[7_/_-1] row-[1_/_7] rounded-md bg-[#f0f0f0] flex items-center justify-center p-4'>
+        <div className='col-[7_/_-1] row-[1_/_-1] md:row-[1_/_7] rounded-md bg-[#f0f0f0] flex items-center justify-center p-4'>
           <img
             src={Image}
             alt=''
@@ -24,14 +26,14 @@ const BottomArrival = () => {
           />
         </div>
 
-        <div className='col-[7_/_10] row-[7_/_-1] rounded-md bg-[#f0f0f0] flex items-center justify-center p-4'>
+        <div className='col-[1_/_-1] row-[11_/_-1] md:col-[7_/_10] md:row-[7_/_-1] rounded-md bg-[#f0f0f0] flex items-center justify-center p-4'>
           <img
             src={Image}
             alt=''
           />
         </div>
 
-        <div className='bg-[#f0f0f0] col-[10_/_-1] row-[7_/_-1] rounded-md flex items-center justify-center p-4'>
+        <div className='bg-[#f0f0f0] col-[1_/_-1] md:col-[10_/_-1] md:row-[7_/_-1] rounded-md flex items-center justify-center p-4'>
           <img
             src={Image1}
             alt=''
