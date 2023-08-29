246413ae49db70bfad1ec860e42085736d9166b9
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-29 22:46:14 +0100

    Working on checkout

--- src/components/molecules/Checkout.tsx
+++ src/components/molecules/Checkout.tsx
@@ -2,7 +2,7 @@ const Checkout = () => {
     return (
       <div className='p-8vw paddTop'>
         <div>
-          <h2>Billing Details</h2>
+          <h2 className="text-[2rem]">Billing Details</h2>
         </div>
       </div>
     );
