2806f1a8cb364dd032c08e05f73fb01d744c0c53
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-30 13:21:21 +0100

    Working on ManageAccount

--- src/components/molecules/manager/index.tsx
+++ src/components/molecules/manager/index.tsx
@@ -1,11 +1,31 @@
 import { Outlet } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+
+import {
+  PiCookingPotLight,
+  PiTelevisionSimpleLight,
+  PiBabyLight,
+} from 'react-icons/pi';
+
+
+import { FaAppleAlt } from 'react-icons/fa';
+
+import { GiLipstick } from 'react-icons/gi';
+
+import { BiHomeAlt2 } from 'react-icons/bi';
 
 const AccountLayout = () => {
+    const Account = [{
+        id: uuidv4(),
+}]
+
   return (
     <div className='grid grid-cols-[_repeat(12,1fr)] px-[_calc(1rem_+_8vw)] gap-4 py-4'>
-      <h2 className='col-[_1/_5]'>Hello sir....</h2>
+      <h2 className='col-[_1/_5] shadow-md rounded-md  bg-white'>
+        Hello sir....
+      </h2>
 
-      <div className='col-[_5/_-1]'>
+      <div className='col-[_5/_-1] shadow-md rounded-md  bg-white'>
         <Outlet />
       </div>
     </div>
