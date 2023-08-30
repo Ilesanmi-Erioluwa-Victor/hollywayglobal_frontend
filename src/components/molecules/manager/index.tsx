e416887113360c01a6185051ecad0f608ed6ab8e
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-30 13:33:41 +0100

    Working on ManageAccount

--- src/components/molecules/manager/index.tsx
+++ src/components/molecules/manager/index.tsx
@@ -60,9 +60,9 @@ const AccountLayout = () => {
 
   return (
     <div className='grid grid-cols-[_repeat(12,1fr)] px-[_calc(1rem_+_8vw)] gap-4 py-4'>
-      <h2 className='col-[_1/_5] shadow-md rounded-md  bg-white'>
-        Hello sir....
-      </h2>
+      <div className='col-[_1/_5] shadow-md rounded-md  bg-white'>
+        
+      </div>
 
       <div className='col-[_5/_-1] shadow-md rounded-md  bg-white'>
         <Outlet />
