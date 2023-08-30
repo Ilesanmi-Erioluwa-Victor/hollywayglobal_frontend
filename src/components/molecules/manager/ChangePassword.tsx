f0907ec6438d89f7dd6d25b4fae918ed78ca7822
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-31 00:44:47 +0100

    Working on changePassword in ManageAccount

--- src/components/molecules/manager/ChangePassword.tsx
+++ src/components/molecules/manager/ChangePassword.tsx
@@ -1,7 +1,18 @@
+import { FieldSet } from 'src/components/atoms';
 
 const ChangePassword = () => {
-  return <div className='p-6'>ChangePassword</div>;
-}
+  return (
+    <div className='p-6'>
+      <FieldSet
+        label={'password'}
+        id={'password'}
+        name='password'
+        onChange={() => console.log()}
+        value=''
+        type='password'
+      />
+    </div>
+  );
+};
 
-export default ChangePassword
-
+export default ChangePassword;
