91b2fe739f23e9ee95b16d680e4238c4a22f25f7
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-30 21:48:05 +0100

    Working on ManageAccount

--- src/components/molecules/manager/ChangeImage.tsx
+++ src/components/molecules/manager/ChangeImage.tsx
@@ -1,13 +1,40 @@
-const ChangeImage = () => {
-   const [selectedImage, setSelectedImage] = useState<File | null>(null);
+import React, { useState } from 'react';
 
-   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-     const selectedFile = event.target.files?.[0];
-     if (selectedFile) {
-       setSelectedImage(selectedFile);
-     }
-   };
-  return <div className='p-6'>ChangeImage</div>;
+const ChangeImage = () => {
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files?.[0];
+    if (selectedFile) {
+      setSelectedImage(selectedFile);
+    }
+  };
+  return (
+    <div className='p-6'>
+      <div className='image-uploader'>
+        {selectedImage ? (
+          <img
+            src={URL.createObjectURL(selectedImage)}
+            alt='Preview'
+          />
+        ) : (
+          <div
+            className='dotted-border'
+            onClick={() => document.getElementById('imageInput')?.click()}
+          >
+            <p>Click to select an image</p>
+          </div>
+        )}
+        <input
+          type='file'
+          id='imageInput'
+          accept='image/*'
+          style={{ display: 'none' }}
+          onChange={handleImageChange}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ChangeImage;
