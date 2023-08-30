2470947f83ac073b329a73cfc8ee9a633cbb8af3
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-31 00:48:08 +0100

    Working on changePassword in ManageAccount

--- src/components/molecules/manager/ChangePassword.tsx
+++ src/components/molecules/manager/ChangePassword.tsx
@@ -3,7 +3,8 @@ import { FieldSet } from '../../../components/atoms';
 const ChangePassword = () => {
   return (
     <div className='p-6'>
-      <h2>Password Settings</h2>
+      <h2 className='text-[1rem] font-[400] mb-3'>Password Settings</h2>
+      <hr />
       <FieldSet
         label={'password'}
         id={'password'}
