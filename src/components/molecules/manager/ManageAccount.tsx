c8c140a629fc191e2b9cfa9dae9c552c4cbe07fa
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-30 14:25:08 +0100

    Working on ManageAccount

--- src/components/molecules/manager/ManageAccount.tsx
+++ src/components/molecules/manager/ManageAccount.tsx
@@ -1,7 +1,27 @@
 const ManageAccount = () => {
   return (
-    <div className="p-6">
-      <h2 className="text-[1.6rem]">Account Overviews</h2>
+    <div className='p-6'>
+      <h2 className='text-[1.6rem]'>Account Overview</h2>
+      <hr />
+      <div className='grid grid-cols-[_repeat(12,1fr)] px-[_calc(1rem_+_8vw)] gap-4 py-[2rem]'>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          nulla soluta impedit asperiores quod enim omnis illo voluptas
+          exercitationem, corporis doloremque quia neque veniam, eaque quaerat
+          quibusdam? Optio nam dolores voluptatibus quod, magni assumenda neque,
+          mollitia fugit inventore architecto perferendis cumque officia at
+          maxime voluptates fugiat blanditiis, laborum qui quidem!
+        </div>
+
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          nulla soluta impedit asperiores quod enim omnis illo voluptas
+          exercitationem, corporis doloremque quia neque veniam, eaque quaerat
+          quibusdam? Optio nam dolores voluptatibus quod, magni assumenda neque,
+          mollitia fugit inventore architecto perferendis cumque officia at
+          maxime voluptates fugiat blanditiis, laborum qui quidem!
+        </div>
+      </div>
     </div>
   );
 };
