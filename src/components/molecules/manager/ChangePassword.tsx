2d4dc412149ad842e798371c27957055a498b5f2
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-31 00:56:20 +0100

    Working on changePassword in ManageAccount

--- src/components/molecules/manager/ChangePassword.tsx
+++ src/components/molecules/manager/ChangePassword.tsx
@@ -1,6 +1,27 @@
+import { useState } from 'react';
 import { FieldSet } from '../../../components/atoms';
 
 const ChangePassword = () => {
+  const [data, setData] = useState({
+    password: '',
+    email: '',
+  });
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setData({ ...data, [name]: value.trim() });
+  };
+
+  const handleInputSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='p-6 flex flex-col gap-4'>
       <h2 className='text-[1.4rem] font-[400] mb-3'>Password Settings</h2>
