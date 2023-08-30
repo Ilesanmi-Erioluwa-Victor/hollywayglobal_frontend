6699c0814bf55be31589de562743a4050d861986
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-30 13:35:01 +0100

    Working on ManageAccount

--- src/components/molecules/manager/index.tsx
+++ src/components/molecules/manager/index.tsx
@@ -7,7 +7,6 @@ import {
   PiBabyLight,
 } from 'react-icons/pi';
 
-
 import { FaAppleAlt } from 'react-icons/fa';
 
 import { GiLipstick } from 'react-icons/gi';
@@ -15,53 +14,58 @@ import { GiLipstick } from 'react-icons/gi';
 import { BiHomeAlt2 } from 'react-icons/bi';
 
 const AccountLayout = () => {
-    const AccountNav = [
-      {
-        id: uuidv4(),
-        name: 'My Account',
-        link: '/user/account/views',
-        icon: <FaAppleAlt />,
-      },
-      {
-        id: uuidv4(),
-        name: 'My Orders',
-        link: '/user/account/orders',
-        icon: <GiLipstick />,
-      },
+  const AccountNav = [
+    {
+      id: uuidv4(),
+      name: 'My Account',
+      link: '/user/account/views',
+      icon: <FaAppleAlt />,
+    },
+    {
+      id: uuidv4(),
+      name: 'My Orders',
+      link: '/user/account/orders',
+      icon: <GiLipstick />,
+    },
 
-      {
-        id: uuidv4(),
-        name: 'My Reviews',
-        link: '/user/account/reviews',
-        icon: <BiHomeAlt2 />,
-      },
+    {
+      id: uuidv4(),
+      name: 'My Reviews',
+      link: '/user/account/reviews',
+      icon: <BiHomeAlt2 />,
+    },
 
-      {
-        id: uuidv4(),
-        name: 'My Address Book',
-        link: '/user/account/address',
-        icon: <PiTelevisionSimpleLight />,
-      },
+    {
+      id: uuidv4(),
+      name: 'My Address Book',
+      link: '/user/account/address',
+      icon: <PiTelevisionSimpleLight />,
+    },
 
-      {
-        id: uuidv4(),
-        name: 'My Password',
-        link: '/user/account/password',
-        icon: <PiBabyLight />,
-      },
+    {
+      id: uuidv4(),
+      name: 'My Password',
+      link: '/user/account/password',
+      icon: <PiBabyLight />,
+    },
 
-      {
-        id: uuidv4(),
-        name: 'Change Profile Image',
-        link: '/user/account/updateImage',
-        icon: <PiCookingPotLight />,
-      },
-    ];
+    {
+      id: uuidv4(),
+      name: 'Change Profile Image',
+      link: '/user/account/updateImage',
+      icon: <PiCookingPotLight />,
+    },
+  ];
 
   return (
     <div className='grid grid-cols-[_repeat(12,1fr)] px-[_calc(1rem_+_8vw)] gap-4 py-4'>
       <div className='col-[_1/_5] shadow-md rounded-md  bg-white'>
-        
+        {AccountNav.map((accNav) => (
+          <div
+            key={accNav.id}
+            className='flex flex-col justify-center'
+          ></div>
+        ))}
       </div>
 
       <div className='col-[_5/_-1] shadow-md rounded-md  bg-white'>
