d67a87fdae45576dae8b3135b9b00e25e0934ac1
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-30 21:46:49 +0100

    Working on ManageAccount

--- src/components/molecules/manager/ChangeImage.tsx
+++ src/components/molecules/manager/ChangeImage.tsx
@@ -1,4 +1,12 @@
 const ChangeImage = () => {
+   const [selectedImage, setSelectedImage] = useState<File | null>(null);
+
+   const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+     const selectedFile = event.target.files?.[0];
+     if (selectedFile) {
+       setSelectedImage(selectedFile);
+     }
+   };
   return <div className='p-6'>ChangeImage</div>;
 };
 
