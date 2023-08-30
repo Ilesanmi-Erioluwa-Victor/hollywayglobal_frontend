3b133141ccb30e858427fbe3da46589c9afc72e3
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-30 13:09:53 +0100

    Working on ManageAccount

--- src/components/molecules/manager/index.tsx
+++ src/components/molecules/manager/index.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 const AccountLayout = () => {
   return (
-    <div>
+    <div className='grid grid-cols-[_repeat(auto-fit,_minmax(_200px,_1fr))] px-[_calc(1rem_+_8vw)]'>
       <h2>Hello sir....</h2>
       <Outlet />
     </div>
