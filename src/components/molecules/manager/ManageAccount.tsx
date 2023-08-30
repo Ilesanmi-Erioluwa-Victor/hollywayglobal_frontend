dec5595dd796508f5cba060f82ad16e4f7968028
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-30 21:18:05 +0100

    Working on ManageAccount

--- src/components/molecules/manager/ManageAccount.tsx
+++ src/components/molecules/manager/ManageAccount.tsx
@@ -1,10 +1,10 @@
 const ManageAccount = () => {
   return (
     <div className='p-6'>
-      <h2 className='text-[1.6rem]'>Account Overview</h2>
+      <h2 className='text-[1.6rem] mb-3'>Account Overview</h2>
       <hr />
       <div className='grid grid-cols-[_repeat(auto-fit,_minmax(200px,1fr))] gap-4 py-[2rem]'>
-        <div className='border p-4 rounded-sm'>
+        <div className='border p-4 rounded-md'>
           <h3 className='text-[1rem] py-2 font-[550] font-[Arial]'>
             ACCOUNT DETAILS
           </h3>
@@ -17,10 +17,13 @@ const ManageAccount = () => {
           maxime voluptates fugiat blanditiis, laborum qui quidem!
         </div>
 
-        <div className='border p-4 rounded-sm'>
+        <div className='border p-4 rounded-md'>
+          <div className="flex justify-between items-center">
+
           <h3 className='text-[1rem] py-2 font-[550] font-[Arial]'>
             ADDRESS BOOK
           </h3>
+          </div>
           <hr />
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae
           nulla soluta impedit asperiores quod enim omnis illo voluptas
