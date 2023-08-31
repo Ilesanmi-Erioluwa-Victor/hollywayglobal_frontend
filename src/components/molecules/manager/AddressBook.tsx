901dad897bb7c1b76cd849cc92b3fe5fc62dbf36
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-31 01:05:32 +0100

    Working on changePassword in ManageAccount

--- src/components/molecules/manager/AddressBook.tsx
+++ src/components/molecules/manager/AddressBook.tsx
@@ -1,5 +1,12 @@
 const AddressBook = () => {
-  return <div className='p-6'>AddressBook</div>;
+  return (
+    <div className='p-6'>
+      <div>
+        <h2>AddressBook</h2>
+        <span>ADD  NEW ADDRESS</span>
+      </div>
+    </div>
+  );
 };
 
 export default AddressBook;
