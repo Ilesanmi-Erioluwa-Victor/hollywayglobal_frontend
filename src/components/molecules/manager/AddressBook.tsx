f07ceb3d8b5c22b7bac15d30bfefc421f26e269a
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-31 01:11:23 +0100

    Working on changePassword in ManageAccount

--- src/components/molecules/manager/AddressBook.tsx
+++ src/components/molecules/manager/AddressBook.tsx
@@ -1,9 +1,11 @@
+import { Link }from "react-router-dom"
+
 const AddressBook = () => {
   return (
     <div className='p-6'>
       <div className='flex items-center justify-between'>
         <h2>AddressBook</h2>
-        <button className='text-white p-2 rounded-sm bg-[#DB4444]'>ADD NEW ADDRESS</button>
+        <Link className='text-white p-2 rounded-sm bg-[#DB4444]' to={"user/account/address/create"}>ADD NEW ADDRESS</Link>
       </div>
     </div>
   );
