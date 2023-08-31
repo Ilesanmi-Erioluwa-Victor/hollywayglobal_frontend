3129df1a460e333b17aa700635fa51278983e1cd
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-08-31 01:08:18 +0100

    Working on changePassword in ManageAccount

--- src/components/molecules/manager/AddressBook.tsx
+++ src/components/molecules/manager/AddressBook.tsx
@@ -1,9 +1,9 @@
 const AddressBook = () => {
   return (
     <div className='p-6'>
-      <div className="flex items-center justify-between">
+      <div className='flex items-center justify-between'>
         <h2>AddressBook</h2>
-        <span className='text-[#DB4444]'>ADD NEW ADDRESS</span>
+        <span className='text-white p-2 rounded-sm bg-[#DB4444]'>ADD NEW ADDRESS</span>
       </div>
     </div>
   );
