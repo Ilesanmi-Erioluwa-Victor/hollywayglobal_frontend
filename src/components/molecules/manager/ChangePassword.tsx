5b477581921db82ff578c641733a63937041a802
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-01 11:57:57 +0100

    Change app routes, route to useRoute hook

--- src/components/molecules/manager/ChangePassword.tsx
+++ src/components/molecules/manager/ChangePassword.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, useState } from 'react';
 import { FieldSet } from '../../../components/atoms';
+import { MdPassword } from 'react-icons/md';
 
 const ChangePassword = () => {
   const [data, setData] = useState({
@@ -27,7 +28,11 @@ const ChangePassword = () => {
       className='p-6 flex flex-col gap-4'
       onSubmit={handleInputSubmit}
     >
-      <h2 className='text-[1.4rem] font-[400] mb-3'>Password Settings</h2>
+      <div className='flex items-center gap-4'>
+        <MdPassword className='text-[2rem]' />
+
+        <h2 className='text-[1.4rem] font-[400]'>Password Settings</h2>
+      </div>
       <hr className='mb-4' />
 
       <FieldSet
