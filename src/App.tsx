5636f318258749d58f8741824066fb93375ca03b
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-01 11:29:23 +0100

    Working on EditProfile

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useRoutes } from 'react-router-dom';
 
 import {
   Footer,
@@ -18,86 +18,89 @@ import { Home, Register, Login, Products } from './pages';
 
 const App = () => {
   // const navigate = useNavigate()
+  const customRoutes = useRoutes([
+    {
+      path: '/',
+      element: <Home />,
+    },
+
+    {
+      element: <Register />,
+      path: '/register',
+    },
+
+    {
+      element: <Login />,
+      path: '/login',
+    },
+
+    {
+      element: <Products />,
+      path: '/products',
+    },
+  ]);
+
+  //       <Route
+  //         element={<Cart />}
+  //         path={'/cart'}
+  //       />
+
+  //       <Route
+  //         element={<Checkout />}
+  //         path={'/checkout'}
+  //       />
+
+  //       <Route
+  //         element={<AccountLayout />}
+  //         path={'/user/account'}
+  //       >
+  //         <Route
+  //           element={<ManageAccount />}
+  //           index
+  //         />
+  //         <Route
+  //           element={<Order />}
+  //           path='orders'
+  //         />
+
+  //         <Route
+  //           element={<Reviews />}
+  //           path='reviews'
+  //         />
+
+  //         <Route
+  //           element={<AddressBook />}
+  //           path='address'
+  //         />
+
+  //         <Route
+  //           element={<ChangePassword />}
+  //           path='password'
+  //         />
+
+  //         <Route
+  //           element={<ChangeImage />}
+  //           path='updateImage'
+  //         />
+
+  //         <Route
+  //           element={<NewAddressBook />}
+  //           path='address/create'
+  //         />
+  //       </Route>
+
+  //       <Route
+  //         element={<h1> Error page</h1>}
+  //         path={'*'}
+  //       />
+  // </Routes>;
+
   return (
     <div className='container-custom bg-slate-100'>
       <div className='overall-layout'>
         <Header />
+        {customRoutes}
 
-        <Routes>
-          <Route
-            element={<Home />}
-            path={'/'}
-          />
-
-          <Route
-            element={<Register />}
-            path={'/register'}
-          />
-
-          <Route
-            element={<Login />}
-            path={'/login'}
-          />
-
-          <Route
-            element={<Products />}
-            path={'/products'}
-          />
-
-          <Route
-            element={<Cart />}
-            path={'/cart'}
-          />
-
-          <Route
-            element={<Checkout />}
-            path={'/checkout'}
-          />
-
-          <Route
-            element={<AccountLayout />}
-            path={'/user/account'}
-          >
-            <Route
-              element={<ManageAccount />}
-              index
-            />
-            <Route
-              element={<Order />}
-              path='orders'
-            />
-
-            <Route
-              element={<Reviews />}
-              path='reviews'
-            />
-
-            <Route
-              element={<AddressBook />}
-              path='address'
-            />
-
-            <Route
-              element={<ChangePassword />}
-              path='password'
-            />
-
-            <Route
-              element={<ChangeImage />}
-              path='updateImage'
-            />
-
-            <Route
-              element={<NewAddressBook />}
-              path='address/create'
-            />
-          </Route>
-
-          <Route
-            element={<h1> Error page</h1>}
-            path={'*'}
-          />
-        </Routes>
         <Footer />
       </div>
     </div>
