d5285237f2f92b50638c4ea9197f14d5c8e0ae0e
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-04 12:56:58 +0100

    Working on Product page

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom"
-import errorPage from "../assets/error-page.svg"
+import { Link } from 'react-router-dom';
+import errorPage from '../assets/error-page.svg';
 
 type ErrorMessage = Record<
   number,
@@ -52,4 +52,4 @@ const ErrorPage = (props: { statusCode: number }): JSX.Element => {
   );
 };
 
-export default ErrorPage
+export default ErrorPage;
