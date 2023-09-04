c5d892cf5756272a989c859cbb329eb2f02f7bb5
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-04 13:04:16 +0100

    Working on Prdouct

--- src/components/molecules/Product.tsx
+++ src/components/molecules/Product.tsx
@@ -1,5 +1,8 @@
 import Image from "../../assets/boom.png"
 import Image2 from "../../assets/dark_800.png"
+import Image3 from "../../assets/error-page.svg"
+import Image4 from "../../assets/boom.png"
+import Image5 from "../../assets/react.svg"
 
 const Product = () => {
   return <div>Product</div>;
