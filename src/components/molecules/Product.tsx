c1d9dd1fa18ba995975abf8ae816be0a1012fcb9
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-04 13:42:10 +0100

    Working on Prdouct

--- src/components/molecules/Product.tsx
+++ src/components/molecules/Product.tsx
@@ -3,14 +3,14 @@ import Image2 from '../../assets/dark_800.png';
 import Image3 from '../../assets/error-page.svg';
 import Image4 from '../../assets/boom.png';
 import Image5 from '../../assets/react.svg';
-import { BsTruck } from "react-icons/bs"
-import { GrPowerCycle } from "react-icons/gr"
+import { BsTruck } from 'react-icons/bs';
+import { GrPowerCycle } from 'react-icons/gr';
 
 const Product = () => {
   const products = [
     {
-          name: 'Havic HV G-92 Gamepad',
-          images : [],
+      name: 'Havic HV G-92 Gamepad',
+      images: [],
       summary:
         'PlayStation 5 Controller Skin High quality vinyl with air channel adhesive for easy bubble free install & mess free removal Pressure sensitive.',
       price: '$192.00',
@@ -29,7 +29,7 @@ const Product = () => {
       ],
     },
   ];
-  return <div>Product</div>;
+  return <div className='p-[_calc(_1rem+_8vw)] flex flex-col gap-y-4 py-4'>Product</div>;
 };
 
 export default Product;
