a2d86ba1cf01437ceed625475c98dcac3dcadb9d
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-05 07:44:31 +0100

    Working on product

--- src/components/molecules/Product.tsx
+++ src/components/molecules/Product.tsx
@@ -97,17 +97,17 @@ const Product = () => {
             </p>
             <section className=' flex w-full items-center mt-2 gap-2 mb-7'>
               <div className=' flex w-full'>
-                <button className='bg-slate-200 py-2 px-4 w-[40%] rounded-sm border-slate-800 border'>
+                <button className='bg-slate-200 py-2 px-4 w-[40%] rounded-sm border-slate-800 border h-[2.5rem]'>
                   -
                 </button>
-                <p className='bg-slate-200 py-2 px-4 w-full border-slate-800 border'>
+                <p className='bg-slate-200 py-2 px-4 w-full border-slate-800 border h-[2.5rem]'>
                   {2}
                 </p>
-                <button className='py-2 px-4 w-[40%] border-slate-800 border-[0.5px] rounded-sm bg-[#DB4444] text-white'>
+                <button className='py-2 px-4 w-[40%] border-slate-800 border-[0.5px] h-[2.5rem] rounded-sm bg-[#DB4444] text-white'>
                   +
                 </button>
               </div>
-              <button className='  w-full py-2 px-4 rounded-sm bg-[#DB4444] text-white'>
+              <button className='  w-full py-2 px-4 rounded-sm bg-[#DB4444] text-white h-[2.5rem]'>
                 Buy Now
               </button>
             </section>
