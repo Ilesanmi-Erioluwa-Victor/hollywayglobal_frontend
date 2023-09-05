59303c560afa90aca41df26646437203ae160556
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-05 11:03:19 +0100

    Working on userStore

--- src/zustand/user/userStore.tsx
+++ src/zustand/user/userStore.tsx
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import axios from 'axios';
 import { UserData } from './types';
 interface UserState {
-  user: null | User; // You can define a User type for your user data
+  user: null | any;
   signUpError: null | string;
   signUpLoading: boolean;
   signUp: (userData: UserData) => Promise<void>;
@@ -15,18 +15,20 @@ const useUserStore = create<UserState>((set) => ({
   signUpLoading: false,
   signUp: async (userData: UserData) => {
     try {
-      set({ signUpLoading: true, signUpError: null });
-      // Make the POST request to your signup endpoint using Axios
-      const response = await axios.post('/api/signup', userData);
+      //   set({ signUpLoading: true, signUpError: null });
+      const response = await axios.post(
+        '/https://hollywayglobadb.onrender.com/signup',
+        userData
+      );
 
       // Assuming your server returns user data upon successful signup
       const user = response.data;
       set({ user, signUpLoading: false });
     } catch (error) {
-      set({
-        signUpError: error.message || 'Signup failed',
-        signUpLoading: false,
-      });
+      //   set({
+      //     signUpError: error.message || 'Signup failed',
+      //     signUpLoading: false,
+      //   });
     }
   },
   clearSignUpError: () => set({ signUpError: null }),
