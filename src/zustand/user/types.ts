7c53d899029278dddd2548f0ba69e94a25646198
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-05 10:57:51 +0100

    Working on useStore

---
+++ src/zustand/user/types.ts
@@ -0,0 +1,6 @@
+export interface userData {
+    firstName: string;
+    lastName: string;
+    password: string;
+    
+}
--- src/zustand/user/userStore.tsx
+++ src/zustand/user/userStore.tsx
@@ -1,4 +1,4 @@
-import create from 'zustand';
+import { create } from 'zustand';
 import axios from 'axios';
 
 interface UserState {
@@ -8,3 +8,28 @@ interface UserState {
   signUp: (userData: UserData) => Promise<void>;
   clearSignUpError: () => void;
 }
+
+const useUserStore = create<UserState>((set) => ({
+  user: null,
+  signUpError: null,
+  signUpLoading: false,
+  signUp: async (userData: UserData) => {
+    try {
+      set({ signUpLoading: true, signUpError: null });
+      // Make the POST request to your signup endpoint using Axios
+      const response = await axios.post('/api/signup', userData);
+
+      // Assuming your server returns user data upon successful signup
+      const user = response.data;
+      set({ user, signUpLoading: false });
+    } catch (error) {
+      set({
+        signUpError: error.message || 'Signup failed',
+        signUpLoading: false,
+      });
+    }
+  },
+  clearSignUpError: () => set({ signUpError: null }),
+}));
+
+export default useUserStore;
