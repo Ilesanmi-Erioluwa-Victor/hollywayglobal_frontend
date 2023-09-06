fa923d45d3f499f7a7c223c6736c8109cbc9524e
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-07 00:21:59 +0100

    Working on login

--- src/services/authService.tsx
+++ src/services/authService.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-useless-catch */
 
-import { registerI } from 'src/types';
+import { registerI, loginI } from 'src/types';
 import { apiClient } from './apiService';
 
 const register = async (userData: registerI) => {
@@ -12,27 +12,9 @@ const register = async (userData: registerI) => {
   }
 };
 
-const post = async (url: string, data = {}) => {
+const login = async (userData: loginI) => {
   try {
-    const response = await apiClient.post(url, data);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-const put = async (url: string, data = {}) => {
-  try {
-    const response = await apiClient.put(url, data);
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
-};
-
-const del = async (url: string) => {
-  try {
-    const response = await apiClient.delete(url);
+    const response = await apiClient.post('user/signup', userData);
     return response.data;
   } catch (error) {
     throw error;
@@ -41,7 +23,5 @@ const del = async (url: string) => {
 
 export default {
   register,
-  post,
-  put,
-  del,
+  login,
 };
