b3e9e7d61c6cdcf156d6480a860c5f1f8147fe40
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-06 16:17:28 +0100

    Working on Dev both client

--- src/services/userApiService.tsx
+++ src/services/userApiService.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-catch */
 import axios from 'axios';
 import { Util } from 'src/utils';
 
@@ -7,3 +8,26 @@ const apiClient = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+const get = async (url: string, params = {}) => {
+  try {
+    const response = await apiClient.get(url, { params });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const post = async (url: string, data = {}) => {
+  try {
+    const response = await apiClient.post(url, data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default {
+    get,
+    post
+};
