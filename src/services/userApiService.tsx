c4a4a818214f111b36ddda2111679f06cfa08a58
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-06 15:44:57 +0100

    Working on userApiService

--- src/services/userApiService.tsx
+++ src/services/userApiService.tsx
@@ -1,1 +1,11 @@
 import axios from 'axios';
+import { Util } from 'src/utils';
+
+const apiClient = axios.create({
+  baseURL: 'https://api.example.com', // Replace with your API base URL
+  timeout: 5000, // Adjust as needed
+  headers: {
+    'Content-Type': 'application/json',
+    // You can set common headers here (e.g., authentication headers)
+  },
+});
