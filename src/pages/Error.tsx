a34545e7be4689af58bece5046afac4084b148f9
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-28 18:26:01 +0100

    working on api

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -17,7 +17,12 @@ const ErrorPage = (): JSX.Element => {
           />
           <h3>Ohh page not found</h3>
           <p>We cant seem to find the page you are looking for</p>
-          <Link to={'/'} className='rounded-sm bg-orange-500 p-3 text-white'>Back Home</Link>
+          <Link
+            to={'/'}
+            className='rounded-sm bg-orange-500 p-3 text-white'
+          >
+            Back Home
+          </Link>
         </div>
       </Wrapper>
     );
