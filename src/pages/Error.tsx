1b7c67ab6d8881dd11de4e736f2cb19792ce7d94
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-28 18:19:56 +0100

    working on api

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,29 +1,33 @@
 import { Link, useRouteError } from 'react-router-dom';
+
 import errorPage from '../assets/error-page.svg';
 
+import Wrapper from 'src/assets/wrappers/ErrorPage';
 
 const ErrorPage = (): JSX.Element => {
+  const error: any = useRouteError();
 
-  return (
-    <div className=' mx-10 font-poppins_Display'>
-      <div className='flex flex-col items-center py-20'>
-        <div className='items-center'>
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
           <img
             src={errorPage}
-            alt='error page'
-            className='items-center'
+            alt='not found'
           />
+          <h3>Ohh page not found</h3>
+          <p>We cant seem to find the page you are looking for</p>
+          <Link to={'/'} className='rounded-sm bg-orange-500 p-3 text-white'>Back Home</Link>
         </div>
-        <div className='flex flex-col text-center w-10/12 lg:w-6/12'>
-      
-        </div>
-        <Link to='/'>
-          <button className='border-[#DB4444] transition-all hover:bg-[#DB4444] hover:text-white border-2 rounded-md px-6 py-4 mt-5 font-medium font-poppins_Display'>
-            Take me home
-          </button>
-        </Link>
+      </Wrapper>
+    );
+  }
+  return (
+    <Wrapper>
+      <div>
+        <h3>Ohh page not found</h3>
       </div>
-    </div>
+    </Wrapper>
   );
 };
 
