c915cc1bf4ca5fb18fdc573d7cae8d4f40ccc9b6
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-28 19:15:57 +0100

    working on register

--- src/components/atoms/Field.tsx
+++ src/components/atoms/Field.tsx
@@ -3,44 +3,32 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 
-interface FieldsType {
-  label: string;
-  id: string;
-  value: string;
+interface FormRowType {
+  labelText: string;
   name: string;
   onChange: any;
   type: string;
-  variant?: any;
 }
 
-const FieldSet: React.FC<FieldsType> = ({
-  label,
-  id,
-  value,
-  onChange,
-  name,
-  type,
-  variant,
-}) => {
+const FormRow = ({ type, name, labelText, onChange }: FormRowType) => {
   return (
-    <Box
-      sx={{
-        width: '100%',
-        maxWidth: '100%',
-      }}
-    >
-      <TextField
-        fullWidth
-        label={label}
-        id={id}
-        name={name}
-        value={value}
-        onChange={onChange}
+    <div className='form-row'>
+      <label
+        htmlFor={name}
+        className='form-label'
+      >
+        {labelText || name}
+      </label>
+      <input
         type={type}
-        variant={variant}
+        id={name}
+        name={name}
+        className='form-input'
+        onChange={onChange}
+        required
       />
-    </Box>
+    </div>
   );
 };
 
-export default FieldSet;
+export default FormRow;
