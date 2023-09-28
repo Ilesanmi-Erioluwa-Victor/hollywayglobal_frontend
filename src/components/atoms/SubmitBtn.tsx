0c60a82a14bb0efc75bda03d52b116b931451640
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-09-28 19:23:30 +0100

    working on register

---
+++ src/components/atoms/SubmitBtn.tsx
@@ -0,0 +1,23 @@
+import { useNavigation } from 'react-router-dom';
+
+interface formBtnType {
+  formBtn?: Boolean;
+  text: string;
+}
+
+const SubmitBtn = ({ formBtn, text }: formBtnType) => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
+  return (
+    <button
+      type='submit'
+      className={`btn btn-block ${formBtn && 'form-btn'}`}
+      disabled={isSubmitting}
+    >
+      {isSubmitting ? 'submitting' : text}
+    </button>
+  );
+};
+
+export default SubmitBtn;
