4448ed0f797fdd1545e238d33c1cf48dd90cd4c8
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-10-03 23:55:48 +0100

    logic working

--- src/components/molecules/manager/address/AddressBook.tsx
+++ src/components/molecules/manager/address/AddressBook.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { Link, Outlet, useMatch } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from 'src/redux/hooks';
 import { getAddressesAction } from 'src/redux/slices/address';
+import { MdOutlineDelete } from 'react-icons/md';
+import { CiEdit } from 'react-icons/ci';
 
 const AddressBook = () => {
   const dispatch = useAppDispatch();
@@ -35,23 +37,44 @@ const AddressBook = () => {
       )}
 
       {!address?.length ? (
-        <h3>No Address yet, add new address</h3>
+        <h3 className='font-semibold text-lg text-center my-auto'>
+          No Address yet, add new address
+        </h3>
       ) : (
         <div className='grid grid-cols-[_repeat(auto-fit,_minmax(300px,_1fr))] gap-4 p-4'>
           {address.map((data: any) => {
             return (
               <div
-                className='shadow-lg px-3 py-4 border rounded-md flex flex-col'
+                className='shadow-lg px-3 py-4 border rounded-md flex flex-col gap-3'
                 key={data.id}
               >
-                <p> {data?.deliveryAddress}</p>
-                <p>{data?.country}</p>
+                <>
+                  <p> {data?.deliveryAddress}</p>
+                  <p>{data?.country}</p>
 
-                <p>{data?.region}</p>
-                <p>{data?.city}</p>
-                <p>
-                  {data?.phone} / {data?.additional && data?.additional}
-                </p>
+                  <p>{data?.region}</p>
+                  <p>{data?.city}</p>
+                  <p>
+                    {data?.phone} /{' '}
+                    {data?.additionalPhone && data?.additionalPhone}
+                  </p>
+                  <hr />
+                  <div className='flex justify-between items-center'>
+                    <button
+                      onClick={data.id}
+                      className='p-2 text-[1.5rem] cursor-pointer transition-all rounded-[50%] hover:bg-green-500 hover:text-white'
+                    >
+                      {' '}
+                      <MdOutlineDelete />
+                    </button>
+                    <button
+                      onClick={data.id}
+                      className='p-1 text-[1.5rem] transition-all cursor-pointer rounded-[50%] hover:bg-green-500 hover:text-white'
+                    >
+                      <CiEdit />
+                    </button>
+                  </div>
+                </>
               </div>
             );
           })}
