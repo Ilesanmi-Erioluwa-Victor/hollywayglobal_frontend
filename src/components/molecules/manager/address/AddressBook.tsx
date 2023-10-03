25eaf8eb43106a4d7fe390a2061507ea2ee505a4
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-10-03 23:20:39 +0100

    logic working

--- src/components/molecules/manager/address/AddressBook.tsx
+++ src/components/molecules/manager/address/AddressBook.tsx
@@ -5,7 +5,7 @@ import { getAddressesAction } from 'src/redux/slices/address';
 
 const AddressBook = () => {
   const dispatch = useAppDispatch();
-  const [address, setAddress] = useState(null);
+  const [address, setAddress] = useState<any>(null);
   const { data } = useAppSelector((state) => state.user);
   useEffect(() => {
     const response = async () => {
@@ -15,6 +15,7 @@ const AddressBook = () => {
     response();
   }, [data, dispatch]);
 
+  console.log(address);
   const isCreateRoute = useMatch('/user/account/address/create');
   return (
     <div className='p-6'>
@@ -30,6 +31,30 @@ const AddressBook = () => {
             </Link>
           </div>
           <hr className='my-4' />
+        </div>
+      )}
+
+      {!address?.length ? (
+        <h3>No Address yet, add new address</h3>
+      ) : (
+        <div className='grid grid-cols-[_repeat(auto-fit,_minmax(300px,_1fr))] gap-4 p-4'>
+          {address.map((data: any) => {
+            return (
+              <div
+                className='shadow-lg px-3 py-4 border rounded-md flex flex-col'
+                key={data.id}
+              >
+                <p> {data?.deliveryAddress}</p>
+                <p>{data?.country}</p>
+
+                <p>{data?.region}</p>
+                <p>{data?.city}</p>
+                <p>
+                  {data?.phone} / {data?.additional && data?.additional}
+                </p>
+              </div>
+            );
+          })}
         </div>
       )}
 
