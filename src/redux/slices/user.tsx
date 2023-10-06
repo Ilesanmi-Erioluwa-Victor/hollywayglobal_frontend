879d0f40ee024c12c4a0902fabf145fab6ea7252
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-10-06 12:28:58 +0100

    done working on change password, a bug to fix to verify current matched before changing

--- src/redux/slices/user.tsx
+++ src/redux/slices/user.tsx
@@ -1,12 +1,14 @@
-import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
-import { apiClient } from 'src/services/apiService';
-import { User } from 'src/types';
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { apiClient } from "src/services/apiService";
+import { RootState } from "src/redux/store";
+import { createAuthHeaders } from "src/utils";
+import { User } from "src/types";
 
 export const registerAction = createAsyncThunk(
-  'users/register',
+  "users/register",
   async (data: any, { rejectWithValue }) => {
     try {
-      const response = await apiClient.post('auth/register', data);
+      const response = await apiClient.post("auth/register", data);
       return response.data;
     } catch (error: any) {
       return rejectWithValue(error.response.data);
@@ -15,10 +17,10 @@ export const registerAction = createAsyncThunk(
 );
 
 export const loginAction = createAsyncThunk(
-  'users/login',
+  "users/login",
   async (data: { email: string; password: string }, { rejectWithValue }) => {
     try {
-      const response = await apiClient.post('auth/login', data);
+      const response = await apiClient.post("auth/login", data);
       return response.data;
     } catch (error: any) {
       return rejectWithValue(error.response.data);
@@ -26,13 +28,32 @@ export const loginAction = createAsyncThunk(
   }
 );
 
+export const changePasswordAction = createAsyncThunk<
+  any,
+  any,
+  { rejectValue: any; state: RootState }
+>("users/change_password", async (password: string, { rejectWithValue, getState }) => {
+  const userData = getState()?.user?.data;
+  try {
+    const response = await apiClient.put(
+      `user/${userData?.id}/password`,
+      { password },
+      createAuthHeaders(userData?.token)
+    );
+    return response.data;
+  } catch (error: any) {
+    console.log(error);
+    return rejectWithValue(error.response.data);
+  }
+});
+
 interface Data {
   id: any;
   token: any;
 }
 
 export const userAction = createAsyncThunk(
-  'users/profile',
+  "users/profile",
   async (data: { id: string; token: string }, { rejectWithValue }) => {
     try {
       const response = await apiClient.get(`user/${data.id}`, {
@@ -47,8 +68,8 @@ export const userAction = createAsyncThunk(
   }
 );
 
-const storedData = localStorage.getItem('userInfo')
-  ? JSON.parse(localStorage.getItem('userInfo') as string)
+const storedData = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo") as string)
   : null;
 
 interface InitialState {
@@ -56,15 +77,17 @@ interface InitialState {
   user: User | null;
   error: any;
   data: Data | null;
+  password?: string | null;
 }
 
 const usersSlices = createSlice({
-  name: 'users',
+  name: "users",
   initialState: {
     user: null,
     isLoading: false,
     error: null,
     data: storedData,
+    password: null,
   } as InitialState,
   reducers: {},
 
@@ -73,14 +96,11 @@ const usersSlices = createSlice({
     builder.addCase(registerAction.pending, (state) => {
       state.isLoading = true;
     });
-    builder.addCase(
-      registerAction.fulfilled,
-      (state, action: PayloadAction<User>) => {
-        state.isLoading = false;
-        state.user = action?.payload;
-        state.error = null;
-      }
-    );
+    builder.addCase(registerAction.fulfilled, (state, action: PayloadAction<User>) => {
+      state.isLoading = false;
+      state.user = action?.payload;
+      state.error = null;
+    });
     builder.addCase(registerAction.rejected, (state, action: any) => {
       state.isLoading = false;
       state.user = null;
@@ -97,15 +117,12 @@ const usersSlices = createSlice({
       state.user = null;
       state.error = null;
     });
-    builder.addCase(
-      loginAction.fulfilled,
-      (state, action: PayloadAction<User | any>) => {
-        state.isLoading = false;
-        state.user = null;
-        state.error = null;
-        state.data = action?.payload;
-      }
-    );
+    builder.addCase(loginAction.fulfilled, (state, action: PayloadAction<User | any>) => {
+      state.isLoading = false;
+      state.user = null;
+      state.error = null;
+      state.data = action?.payload;
+    });
     builder.addCase(loginAction.rejected, (state, action: any) => {
       state.isLoading = false;
       state.user = null;
@@ -121,17 +138,33 @@ const usersSlices = createSlice({
     builder.addCase(userAction.pending, (state) => {
       state.isLoading = true;
     });
-    builder.addCase(
-      userAction.fulfilled,
-      (state, action: PayloadAction<User>) => {
-        state.isLoading = false;
-        state.user = action?.payload;
-        state.error = null;
-      }
-    );
+    builder.addCase(userAction.fulfilled, (state, action: PayloadAction<User>) => {
+      state.isLoading = false;
+      state.user = action?.payload;
+      state.error = null;
+    });
     builder.addCase(userAction.rejected, (state, action: any) => {
       state.isLoading = false;
       state.user = null;
+      if (action?.payload) {
+        state.error = action?.payload?.message;
+      } else {
+        state.error = action?.error;
+      }
+    });
+
+    // change password
+    builder.addCase(changePasswordAction.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(changePasswordAction.fulfilled, (state, action: PayloadAction<string>) => {
+      state.isLoading = false;
+      state.password = action?.payload;
+      state.error = null;
+    });
+    builder.addCase(changePasswordAction.rejected, (state, action: any) => {
+      state.isLoading = false;
+      state.password = null;
       if (action?.payload) {
         state.error = action?.payload?.message;
       } else {
