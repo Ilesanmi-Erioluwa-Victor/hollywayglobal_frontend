e9d91ff47695a88aebaad3db3a6f274e7c0b88c9
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-10-06 10:02:25 +0100

    modified app

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -2,17 +2,13 @@ import { UserAuth } from '../auth/User';
 import MenuNav from './Navigation/Menu';
 import { useAppSelector } from 'src/redux/hooks';
 const Header = (): JSX.Element => {
-  UserAuth().userInfo;
+  const userInfo = UserAuth().userInfo;
   const user = useAppSelector((state) => state.user.user);
 
   return (
     <>
       <header className='py-[1rem] w-[100%] bg-white'>
-        {!user?.data && user?.status !== 'success' ? (
-          <MenuNav />
-        ) : (
-          <MenuNav user={user?.data} />
-        )}
+        {!userInfo ? <MenuNav /> : <MenuNav user={user?.data} />}
       </header>
     </>
   );
