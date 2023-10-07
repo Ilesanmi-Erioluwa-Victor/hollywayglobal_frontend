47d7de8dec6567497ac1a436cdeb0ae105009834
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-10-07 12:19:35 +0100

    done working on account index

--- src/components/molecules/manager/ManageAccount.tsx
+++ src/components/molecules/manager/ManageAccount.tsx
@@ -1,45 +1,24 @@
-import { BiPencil } from 'react-icons/bi';
-import User from 'src/components/auth/User';
+import { useAppSelector } from 'src/redux/hooks';
 
 const ManageAccount = () => {
-  // const { storedUser } = User();
+  const { user, isLoading } = useAppSelector((state) => state.user);
+  const data = user?.data;
+  if (isLoading) {
+    return;
+  }
   return (
-    // <div className='p-6'>
-    //   <h2 className='text-[1.6rem] mb-3'>Account Overview</h2>
-    //   <hr />
-    //   <div className='grid grid-cols-[_repeat(auto-fit,_minmax(200px,1fr))] gap-4 py-[2rem]'>
-    //     <div className='border p-4 rounded-md'>
-    //       <h3 className='text-[1rem] py-2 font-[500] font-[Arial] mb-2'>
-    //         ACCOUNT DETAILS
-    //       </h3>
-    //       <hr className='mb-3' />
-    //       <div className='flex flex-col gap-2'>
-    //         <p className='text-[0.9rem] font-[500]'>
-    //           {storedUser?.firstName} {storedUser?.lastName}
-    //         </p>
+    <div className='p-4'>
+      <div className='shadow-lg p-4 border rounded-md'>
+        <hr className='my-4' />
+        <section className='flex gap-5 mb-4'>
+          <h2>{data?.firstName}</h2>
+          <h2>{data?.lastName}</h2>
+        </section>
+        <h2 className='mb-4'>{data?.email}</h2>
 
-    //         <p className='font-[500] text-[0.9rem] opacity-50'>
-    //           {storedUser?.email}
-    //         </p>
-    //         <p className='font-[500] text-[0.9rem]'>{storedUser?.mobile}</p>
-    //       </div>
-    //     </div>
-
-    //     <div className='border p-4 rounded-md'>
-    //       <div className='flex justify-between items-center'>
-    //         <h3 className='text-[1rem] py-2 font-[500] font-[Arial] mb-2'>
-    //           ADDRESS BOOK
-    //         </h3>
-    //         <span className='text-[1.4rem] text-orange-500 cursor-pointer hover:bg-orange-200 p-2 rounded-[50%] transition-all'>
-    //           <BiPencil />
-    //         </span>
-    //       </div>
-    //       <hr className='mb-3' />
-    //       <p className='text-[0.9rem]'>Your default shipping address:</p>
-    //     </div>
-    //   </div>
-    // </div>
-    <h1>Hello from ManageAccount</h1>
+        <p>{data?.mobile}</p>
+      </div>
+    </div>
   );
 };
 
