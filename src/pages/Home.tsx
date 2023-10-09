154a60284415f9638d2feacb0a5b0ca00e47acde
Ilesanmi-Erioluwa-Victor <ilesanmierioluwavictor@gmail.com>
2023-10-09 20:59:09 +0100

    worked on bottom

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import { AiOutlineCustomerService } from 'react-icons/ai';
+import { useEffect, useState } from 'react';
 
-import { BsTruck, BsShieldCheck } from 'react-icons/bs';
+import { FaCheck, FaMoneyCheck, FaTruck, FaShoppingCart } from 'react-icons/fa';
 
 import {
   Hero,
@@ -10,32 +10,56 @@ import {
   Boom,
   BottomArrival,
 } from '../components';
-import { apiClient } from 'src/services/apiService';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 import { GeneralLayouts } from 'src/layouts/GeneralLayouts';
+import { useAppSelector, useAppDispatch } from 'src/redux/hooks';
+import { productsAction } from 'src/redux/slices/product';
 
 const remarks = [
   {
-    name: '24/7 CUSTOMER SERVICE',
-    icon: <AiOutlineCustomerService />,
-    sum: 'Friendly 24/7 customer support',
+    bigIcon: <FaCheck />,
+    name: 'Find your product',
+    sum: 'Select from our varieties of fresh products',
   },
 
   {
-    name: ' FREE AND FAST DELIVERY',
-    icon: <BsTruck />,
-    sum: 'Free delivery for all orders over $140',
+    name: ' Add product to Cart',
+    bigIcon: <FaShoppingCart />,
+    sum: 'Add varieties of our products to your cart',
   },
 
   {
-    name: 'MONEY BACK GUARANTEE',
-    icon: <BsShieldCheck />,
-    sum: 'We reurn money within 30 days',
+    name: 'Make Payment',
+    bigIcon: <FaMoneyCheck />,
+    sum: 'Seamless payment via your debit cards',
+  },
+
+  {
+    name: 'Get your delivery',
+    bigIcon: <FaTruck />,
+    sum: 'We package and deliver your orders 24/7',
   },
 ];
 
 const Home = () => {
+  const [product, setProduct] = useState([]);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const data = async () => {
+      const result: any = await dispatch(productsAction());
+      setProduct(result?.payload?.data);
+    };
+
+    data();
+  }, [dispatch]);
+
+  const { isLoading } = useAppSelector((state) => state.product);
+
+  if(isLoading) return
+
+
   return (
     <GeneralLayouts>
       <div className='padd flex flex-col gap-y-4 py-4'>
@@ -46,19 +70,21 @@ const Home = () => {
         <Boom />
         <BottomArrival />
         <div className='p-8vw'>
-          <div className='grid grid-cols-[_repeat(_auto-fit,_minmax(200px,1fr))] md:px-[5rem] flex-col md:flex-row gap-4 flex-wrap'>
+          <h2 className='text-center text-[2rem] font-semibold mb-8'>
+            HOW HOLLYWAYGLOBAL WORKS
+          </h2>
+          <div className='grid grid-cols-[_repeat(_auto-fit,_minmax(200px,1fr))] gap-5'>
             {remarks.map((writeUps) => (
               <div
                 key={writeUps.name}
-                className='flex flex-col items-center gap-3 mb-2 cursor-pointer p-5 hover:bg-green-500 hover:text-white shadow-sm rounded-md transition-all'
+                className='flex items-center justify-center text-center py-[3rem] px-[15px] cursor-pointer gap-4 rounded-sm shadow-sm bg-green-500 text-white hover:bg-green-600 transition-all'
               >
-                <h2 className='bg-[#2F2E30] p-2 rounded-[50%] '>
-                  <span className='bg-black text-white block p-3 rounded-[50%] text-lg'>
-                    {writeUps.icon}
-                  </span>
-                </h2>
-                <h4 className='text-base font-[600] '>{writeUps.name}</h4>
-                <p className=''>{writeUps.sum}</p>
+                <span className='text-[2rem]'>{writeUps.bigIcon}</span>
+
+                <div className=''>
+                  <h4 className=' text-lg font-medium py-3'>{writeUps.name}</h4>
+                  <p className=''>{writeUps.sum}</p>
+                </div>
               </div>
             ))}
           </div>
