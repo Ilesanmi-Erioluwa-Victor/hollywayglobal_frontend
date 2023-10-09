046d105e51814ef2b63f9d824a30fc149f1398d8
ayomikuolatunji <ayomikuolatunji@gmail.com>
2023-10-09 11:41:56 +0100

    working on products

--- src/redux/slices/product.tsx
+++ src/redux/slices/product.tsx
@@ -2,18 +2,18 @@ import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 import { apiClient } from "src/services/apiService";
 import { RootState } from "src/redux/store";
 import { InitialStateProduct } from "src/types";
-export const changePasswordAction = createAsyncThunk<
-  any,
-  any,
-  { rejectValue: any; state: RootState }
->("products", async ({ rejectWithValue }) => {
-  try {
-    const response = await apiClient.get("products");
-    return response.data;
-  } catch (error: any) {
-    return rejectWithValue(error.response.data);
+
+export const productsAction = createAsyncThunk<any, any, { rejectValue: any; state: RootState }>(
+  "products",
+  async ({ rejectWithValue }) => {
+    try {
+      const response = await apiClient.get("products");
+      return response.data;
+    } catch (error: any) {
+      return rejectWithValue(error.response.data);
+    }
   }
-});
+);
 
 const productSlice = createSlice({
   name: "products",
@@ -23,7 +23,26 @@ const productSlice = createSlice({
     isLoading: false,
   } as InitialStateProduct,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    //   get products
+    builder.addCase(productsAction.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(productsAction.fulfilled, (state, action: PayloadAction<any>) => {
+      state.isLoading = false;
+      state.product = action?.payload;
+      state.error = null;
+    });
+    builder.addCase(productsAction.rejected, (state, action: any) => {
+      state.isLoading = false;
+      state.product = null;
+      if (action?.payload) {
+        state.error = action?.payload?.message;
+      } else {
+        state.error = action?.error;
+      }
+    });
+  },
 });
 
 export default productSlice.reducer;
